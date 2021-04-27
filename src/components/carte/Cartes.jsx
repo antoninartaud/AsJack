5e54cfc9451edfebb7f2f661b0e2a597ecfcbcaf
antonin <antoninartaud@hotmail.fr>
2021-04-27 15:36:09 +0200

    Point 1 ok

---
+++ src/components/carte/Cartes.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Cartes extends React.Component {
+  render() {
+    return (
+      <div className='cartes'>
+        {this.props.cardList.map(function (carteFaceValue) {
+          const url =
+            'https://deckofcardsapi.com/static/img/' +
+            carteFaceValue +
+            '.' +
+            'png';
+          return <img src={url} style={{ height: 150, width: 100 }} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default Cartes;
